22c47242b5e1d32ba40329db43265aaad54cff65
Grace <gchen20@stuy.edu>
2021-01-07 02:26:46 -0500

    Created get and set methods for all three variables

--- Node.java
+++ Node.java
@@ -5,6 +5,33 @@ public class Node {
   public Node(String value){
     data = value;
   }
+
+  public String setData(String s) {
+    String ret = data;
+    data = s;
+    return ret;
+  }
+  public String getData() {
+    return data;
+  }
+
+  public Node setNext(Node n) {
+    Node ret = next;
+    next = n;
+    return ret;
+  }
+  public Node getNext() {
+    return next;
+  }
+
+  public Node setPrev(Node n) {
+    Node ret = prev;
+    prev = n;
+    return ret;
+  }
+  public Node getPrev() {
+    return prev;
+  }
  //write get/set methods for all three instance variables.
 
 }
